cb478d6b3ba608f605b7982c77688907325c1921
dixy991 <86074150+dixy991@users.noreply.github.com>
2021-08-31 00:11:45 +0200

    changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,7 @@ export class AppComponent implements OnInit {
   leftOpen: boolean = false;
   rightOpen: boolean = false;
   http: HttpClient;
-  endpoint = 'http://localhost:8000/mail.php';
+  endpoint = 'https://dijana-lazic.herokuapp.com/mail.php';
 
   constructor(http: HttpClient, private projectsService:ProjectsService) {
     this.http = http;
